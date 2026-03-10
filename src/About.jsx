f391e243a3b5b908fee94b5024108586c2b9ff94
Nigel Anthony Thomas <restaurantbarmanager@gmail.com>
2026-03-10 18:55:09 +0530

    Create About.jsx

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>About Us</h1>
+      <p>Coming soon. Learn about JobLynk.</p>
+    </div>
+  );
+}
+
+export default About;
