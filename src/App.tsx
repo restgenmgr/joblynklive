284cd7e85eabbee70012f1aea11e345ee6fd815c
Nigel Anthony Thomas <restaurantbarmanager@gmail.com>
2026-03-16 16:47:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BrainChain from './BrainChain';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -13,18 +13,26 @@ import Disclaimer from './pages/Disclaimer';
 function App() {
   return (
     <Router>
-      <BrainChain />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/payportal" element={<PayPortal />} />
-        <Route path="/resumetools" element={<ResumeTools />} />
-        <Route path="/employersdata" element={<EmployersData />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/disclaimer" element={<Disclaimer />} />
-      </Routes>
+      <div style={{ display: 'flex' }}>
+        <BrainChain />
+        <div style={{ 
+          marginLeft: '150px',  // Make room for the sidebar
+          flex: 1,
+          padding: '20px'
+        }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/payportal" element={<PayPortal />} />
+            <Route path="/resumetools" element={<ResumeTools />} />
+            <Route path="/employersdata" element={<EmployersData />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/disclaimer" element={<Disclaimer />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
