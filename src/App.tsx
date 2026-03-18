fa2377df152603f07b4aa51de1df38e248610ab1
Nigel Anthony Thomas <restaurantbarmanager@gmail.com>
2026-03-18 09:29:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BrainGrid from "./BrainGrid";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Pricing from "./pages/Pricing";
-import PayPortal from "./payportal";
-import ResumeTools from "./resumebuilder";
-import EmployersData from "./employerportal";
-import SignUp from "./samplesvault";
-import Support from "./pages/Support";
-import Disclaimer from "./disclaimer";
-
 function App() {
-  return (
-    <Router>
-      <div style={{ display: 'flex', minHeight: '100vh' }}>
-        <BrainGrid />
-        <main style={{ 
-          flex: 1, 
-          marginLeft: '180px',
-          width: 'calc(100% - 180px)',
-          overflowX: 'hidden',
-          backgroundColor: '#f8fafc',
-          padding: '20px'
-        }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/payportal" element={<PayPortal />} />
-            <Route path="/resumetools" element={<ResumeTools />} />
-            <Route path="/employersdata" element={<EmployersData />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/support" element={<Support />} />
-            <Route path="/disclaimer" element={<Disclaimer />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+  return <h1>APP WORKING 🚀</h1>;
 }
 
 export default App;
