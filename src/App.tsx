eb978535fa60289cc0fad46c0ee386b66f5ff3b3
Nigel Anthony Thomas <restaurantbarmanager@gmail.com>
2026-03-18 20:22:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Jobs from "./pages/Jobs";
-import MapView from "./pages/MapView";
-import Contact from "./pages/Contact";
-import Pricing from "./pages/Pricing";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav style={{ padding: "10px", background: "#222" }}>
-        <Link to="/" style={{ margin: 10, color: "white" }}>Home</Link>
-        <Link to="/jobs" style={{ margin: 10, color: "white" }}>Jobs</Link>
-        <Link to="/map" style={{ margin: 10, color: "white" }}>Map</Link>
-        <Link to="/contact" style={{ margin: 10, color: "white" }}>Contact</Link>
-        <Link to="/pricing" style={{ margin: 10, color: "white" }}>Pricing</Link>
-      </nav>
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/map" element={<MapView />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
