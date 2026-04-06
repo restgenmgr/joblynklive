fe063959ff65b8470a0749c9ab1c206c15c6d95c
NAPT1952 <napt1952@gmail.com>
2026-04-07 00:18:10 +0530

    emergency: restore site after blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
@@ -9,6 +9,4 @@ if (root) {
       <App />
     </React.StrictMode>
   )
-} else {
-  console.error('Root element not found')
 }
