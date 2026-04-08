c054f894952752c32885c535d94b32ffe4fcc09e
NAPT1952 <napt1952@gmail.com>
2026-04-08 18:08:06 +0530

    fix: restore working static homepage, remove React build

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
