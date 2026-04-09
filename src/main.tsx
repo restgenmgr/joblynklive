dabec8736011ab1e43f06968a5c209f2c3211ae0
NAPT1952 <napt1952@gmail.com>
2026-04-09 10:36:37 +0530

    deploy: smart rebuild with auto-detected assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,9 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
-import './index.css';
+﻿import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>
-  );
-} else {
-  console.error('Root element not found');
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
